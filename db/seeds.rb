7509f6722caf6890b772e3e449304abc4a76a7eb
Yossapas <lucifer_ap@hotmail.com>
2010-10-06 02:24:13 -0700

    create restaurant & hotel in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,3 +20,32 @@ Restaurant.create(
 :address => "3280 Peachtree road. NE, Suite 140B, Atlanta, GA 30305", 
 :phone => "3280 Peachtree road. NE, Suite 140B, Atlanta, GA 30305"
 )
+
+Restaurant.create(
+:name => "Whits", 
+:star => 5,
+:description => "Average price*: $ 90", 
+:address => "21 Abingdon Road, London W8 6AH, England", 
+:phone => "+44 (0)20 7938 1122"
+)
+
+Hotel.create(
+:name => "Elysian", 
+:star => 5, 
+:address => "11 East Walton, Chicago, IL 60611", 
+)
+
+Hotel.create(
+:name => "Hotel Palomar Chicago", 
+:star => 4, 
+:address => "505 North State Street, Chicago, IL 60654", 
+:phone => "(877) 731-0505"
+)
+
+Hotel.create(
+:name => "Trump International Hotel & Tower Chicago", 
+:star => 4, 
+:address => "401 N. Wabash Avenue, Chicago, IL 60611", 
+:email => "", 
+:phone => "877-458-7867"
+)
